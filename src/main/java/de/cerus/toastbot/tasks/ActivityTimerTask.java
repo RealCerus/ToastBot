d2eb2aa2249af7314a7ede9a4cc8f2bac74a3671
Cerus <zocks1703@web.de>
2019-04-18 13:09:58 +0200

    Added member count to the presence task thing

--- src/main/java/de/cerus/toastbot/tasks/ActivityTimerTask.java
+++ src/main/java/de/cerus/toastbot/tasks/ActivityTimerTask.java
@@ -16,12 +16,13 @@ import net.dv8tion.jda.api.managers.Presence;
 import javax.annotation.Nonnull;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class ActivityTimerTask extends TimerTask {
 
-    public static Timer startNew(JDA jda, Settings settings){
+    public static Timer startNew(JDA jda, Settings settings) {
         Timer timer = new Timer();
-        timer.scheduleAtFixedRate(new ActivityTimerTask(jda, settings), 0L, 15*1000L);
+        timer.scheduleAtFixedRate(new ActivityTimerTask(jda, settings), 0L, 15 * 1000L);
         return timer;
     }
 
@@ -41,7 +42,9 @@ public class ActivityTimerTask extends TimerTask {
             default:
             case 0:
                 int guilds = jda.getGuilds().size();
-                presence.setActivity(Activity.watching(guilds+(guilds == 1 ? " guild" : " guilds")));
+                AtomicLong members = new AtomicLong();
+                jda.getGuilds().forEach(guild -> guild.getMembers().stream().filter(member -> !member.getUser().isBot()).forEach(member -> members.getAndIncrement()));
+                presence.setActivity(Activity.watching(guilds + (guilds == 1 ? " guild" : " guilds") + " with " + members.get() + (members.get() == 1 ? " member" : " members")));
                 step++;
                 break;
             case 1:
@@ -49,11 +52,11 @@ public class ActivityTimerTask extends TimerTask {
                 step++;
                 break;
             case 2:
-                presence.setActivity(Activity.listening(settings.getCommandPrefix()+"<command>"));
+                presence.setActivity(Activity.listening(settings.getCommandPrefix() + "<command>"));
                 step++;
                 break;
             case 3:
-                presence.setActivity(Activity.playing("on version "+ AppPropertiesReader.getVersion()));
+                presence.setActivity(Activity.playing("on version " + AppPropertiesReader.getVersion()));
                 step = 0;
                 break;
         }
